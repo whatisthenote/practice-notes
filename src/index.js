ecf17a481c4c1970fa95171e3144e2f0620f8ec7
Gabriel <jgj_junior@yahoo.com.br>
2019-07-08 18:29:48 -0300

    practice

--- src/index.js
+++ src/index.js
@@ -1,39 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-class Layout extends React.Component {
+const API = "https://hn.algolia.com/api/v1/search?query=";
+const QUERY = "java";
+
+class App extends Component {
 	constructor() {
 		super();
-		this.state = {
-			title: "Welcome"
-		};
+		this.state = { data: {} };
+	}
+	componentDidMount() {
+		fetch(API + QUERY)
+			.then(resp => resp.json())
+			.then(data => this.setState({ data: data }));
 	}
 	render() {
-		setTimeout(() => {
-			this.setState({ title: "Welcome 2" });
-		}, 1000);
-		return (
-			<div>
-				<Header title={this.state.title} name="NAME" />
-				<Header title={"OTHER TITLE"} name="NAME" />
-				<div>HELLO </div>
-			</div>
-		);
-	}
-}
-class Header extends React.Component {
-	render() {
-		return (
-			<div>
-				<Title title={this.props.title} />
-			</div>
-		);
-	}
-}
-class Title extends React.Component {
-	render() {
-		return <h1>{this.props.title}</h1>;
+		const { data } = this.state
+		const { hits } = data
+		return <div>{console.log(hits)}</div>;
 	}
 }
 
-ReactDOM.render(<Layout />, document.getElementById("root"));
+// class App extends Component {
+// 	constructor(props) {
+// 		super(props);
+
+// 		this.state = {
+// 			hits: []
+// 		};
+// 	}
+// 	componentDidMount() {
+// 		fetch(API + DEFAULT_QUERY)
+// 			.then(response => response.json())
+// 			.then(data => this.setState({ hits: data.hits }));
+// 	}
+// 	render() {
+// 		const { hits } = this.state;
+
+// 		return (
+// 			<ul>
+// 				{hits.map(hit => (
+// 					<li key={hit.objectID}>
+// 						<a href={hit.url}>{hit.title}</a>
+// 					</li>
+// 				))}
+// 			</ul>
+// 		);
+// 	}
+// }
+
+ReactDOM.render(<App />, document.getElementById("root"));
