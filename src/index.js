81daa359871b00f9ccac449526200a9c94818ddf
Gabriel <jgj_junior@yahoo.com.br>
2019-07-09 14:11:09 -0300

    cathing errors

--- src/index.js
+++ src/index.js
@@ -7,24 +7,54 @@ const QUERY = "java";
 class App extends Component {
 	constructor() {
 		super();
-		this.state = { data: [] };
+		this.state = { data: [], isLoading: false };
 	}
 	componentDidMount() {
+		this.setState({ isLoading: true });
 		fetch(API + QUERY)
 			.then(resp => resp.json())
-			.then(data => this.setState({ data: data.hits }));
+			.then(data => this.setState({ data: data.hits, isLoading: false }));
 	}
 	render() {
-		const { data } = this.state;
+		const { data, isLoading } = this.state;
+		if (isLoading) return <p>Loading...</p>;
 		return (
-			<ul>
-				{data.map(hit => (
-					<li key={hit.objectID}>
-						<a href={hit.url}>{hit.title}</a>
-					</li>
-				))}
-			</ul>
+			<div>
+				<h1>LIST</h1>
+				<Error>
+					<List data={data} />
+				</Error>
+			</div>
 		);
+	}
+}
+function List({ data }) {
+	if (true) {
+		throw new Error("error");
+	}
+	return (
+		<ul>
+			{data.map(hit => (
+				<li key={hit.objectID}>
+					<a href={hit.url}>{hit.title}</a>
+				</li>
+			))}
+		</ul>
+	);
+}
+class Error extends Component {
+	constructor() {
+		super();
+		this.state = { error: false };
+	}
+	componentDidCatch() {
+		this.setState({ error: true });
+	}
+	render() {
+		if (this.state.error) {
+			return <p>:(</p>;
+		}
+		return this.props.children;
 	}
 }
 
