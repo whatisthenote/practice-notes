1659fff643923333ad8b53e9205c6d1401fb0f8e
Gabriel <jgj_junior@yahoo.com.br>
2019-07-09 13:19:20 -0300

    update fetch

--- src/index.js
+++ src/index.js
@@ -18,16 +18,43 @@ class App extends Component {
 		const { data } = this.state;
 		return (
 			<ul>
-				{data.map(hit => {
-					return (
-						<li key={hit.objectID}>
-							<a href={hit.url}>{hit.title}</a>
-						</li>
-					);
-				})}
+				{data.map(hit => (
+					<li key={hit.objectID}>
+						<a href={hit.url}>{hit.title}</a>
+					</li>
+				))}
+				)}
 			</ul>
 		);
 	}
 }
+
+// class App extends Component {
+// 	constructor(props) {
+// 		super(props);
+
+// 		this.state = {
+// 			hits: []
+// 		};
+// 	}
+// 	componentDidMount() {
+// 		fetch(API + DEFAULT_QUERY)
+// 			.then(response => response.json())
+// 			.then(data => this.setState({ hits: data.hits }));
+// 	}
+// 	render() {
+// 		const { hits } = this.state;
+
+// 		return (
+// 			<ul>
+// 				{hits.map(hit => (
+// 					<li key={hit.objectID}>
+// 						<a href={hit.url}>{hit.title}</a>
+// 					</li>
+// 				))}
+// 			</ul>
+// 		);
+// 	}
+// }
 
 ReactDOM.render(<App />, document.getElementById("root"));
