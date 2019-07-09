f59d3d977e99e838288ddd5806b6ee5ddb5de65d
Gabriel <jgj_junior@yahoo.com.br>
2019-07-09 13:20:11 -0300

    update

--- src/index.js
+++ src/index.js
@@ -23,38 +23,9 @@ class App extends Component {
 						<a href={hit.url}>{hit.title}</a>
 					</li>
 				))}
-				)}
 			</ul>
 		);
 	}
 }
-
-// class App extends Component {
-// 	constructor(props) {
-// 		super(props);
-
-// 		this.state = {
-// 			hits: []
-// 		};
-// 	}
-// 	componentDidMount() {
-// 		fetch(API + DEFAULT_QUERY)
-// 			.then(response => response.json())
-// 			.then(data => this.setState({ hits: data.hits }));
-// 	}
-// 	render() {
-// 		const { hits } = this.state;
-
-// 		return (
-// 			<ul>
-// 				{hits.map(hit => (
-// 					<li key={hit.objectID}>
-// 						<a href={hit.url}>{hit.title}</a>
-// 					</li>
-// 				))}
-// 			</ul>
-// 		);
-// 	}
-// }
 
 ReactDOM.render(<App />, document.getElementById("root"));
