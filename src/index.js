ff577546906a9d127ec307056f46a510df36d16f
Gabriel <jgj_junior@yahoo.com.br>
2019-07-09 13:17:09 -0300

    practice

--- src/index.js
+++ src/index.js
@@ -7,46 +7,27 @@ const QUERY = "java";
 class App extends Component {
 	constructor() {
 		super();
-		this.state = { data: {} };
+		this.state = { data: [] };
 	}
 	componentDidMount() {
 		fetch(API + QUERY)
 			.then(resp => resp.json())
-			.then(data => this.setState({ data: data }));
+			.then(data => this.setState({ data: data.hits }));
 	}
 	render() {
-		const { data } = this.state
-		const { hits } = data
-		return <div>{console.log(hits)}</div>;
+		const { data } = this.state;
+		return (
+			<ul>
+				{data.map(hit => {
+					return (
+						<li key={hit.objectID}>
+							<a href={hit.url}>{hit.title}</a>
+						</li>
+					);
+				})}
+			</ul>
+		);
 	}
 }
-
-// class App extends Component {
-// 	constructor(props) {
-// 		super(props);
-
-// 		this.state = {
-// 			hits: []
-// 		};
-// 	}
-// 	componentDidMount() {
-// 		fetch(API + DEFAULT_QUERY)
-// 			.then(response => response.json())
-// 			.then(data => this.setState({ hits: data.hits }));
-// 	}
-// 	render() {
-// 		const { hits } = this.state;
-
-// 		return (
-// 			<ul>
-// 				{hits.map(hit => (
-// 					<li key={hit.objectID}>
-// 						<a href={hit.url}>{hit.title}</a>
-// 					</li>
-// 				))}
-// 			</ul>
-// 		);
-// 	}
-// }
 
 ReactDOM.render(<App />, document.getElementById("root"));
