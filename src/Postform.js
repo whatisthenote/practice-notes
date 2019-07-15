df87119b00c4f9109b03a878d510417a46602fc3
Gabriel <jgj_junior@yahoo.com.br>
2019-07-14 22:32:14 -0300

    submiting form

--- src/Postform.js
+++ src/Postform.js
@@ -1,37 +1,32 @@
 import React, { Component } from "react";
 
 export default class Postform extends Component {
-	state = {
-		fname: "",
-		lname: "",
-		password: ""
-	};
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: "",
+			password: ""
+		};
+	}
 	change = e => {
 		this.setState({ [e.target.name]: e.target.value });
 	};
 	submitForm = e => {
 		e.preventDefault();
-		this.props.submit(this.state);
 		this.setState({
-			fname: "",
-			lname: "",
+			username: "",
 			password: ""
 		});
+		console.log(this.state);
 	};
 	render() {
 		return (
 			<div>
 				<form>
 					<input
-						name="fname"
-						placeholder="first name"
-						value={this.state.fname}
-						onChange={e => this.change(e)}
-					/>
-					<input
-						name="lname"
-						placeholder="last name"
-						value={this.state.lname}
+						name="username"
+						placeholder="username"
+						value={this.state.username}
 						onChange={e => this.change(e)}
 					/>
 					<input
@@ -46,41 +41,3 @@ export default class Postform extends Component {
 		);
 	}
 }
-
-// import React from "react";
-
-// class Postform extends React.Component {
-// 	constructor(props) {
-// 		super(props);
-// 		this.state = {
-// 			firstName: "",
-// 			age: ""
-// 		};
-// 	}
-// 	handleFirstNameChange = event => {
-// 		const firstName = event.target.value;
-// 		this.setState({ firstName: firstName });
-// 	};
-// 	handleAgeChange = event => {
-// 		const age = event.target.value;
-// 		this.setState({ age: age });
-// 	};
-// 	handleSubmit = event => {
-// 		event.preventDefault();
-// 		const { firstName, age } = this.state;
-// 		console.log(`A first name was submitted: ${firstName}. An age was submitted: ${age}`);
-// 	};
-// 	render() {
-// 		return (
-// 			<form submitForm={this.handleSubmit}>
-// 				First Name:
-// 				<input name="firstName" type="text" value={this.state.firstName} onChange={this.handleFirstNameChange} />
-// 				Age:
-// 				<input name="age" type="number" value={this.state.age} onChange={this.handleAgeChange} />
-// 				<input type="submit" value="Submit" />
-// 			</form>
-// 		);
-// 	}
-// }
-
-// export default Postform;
