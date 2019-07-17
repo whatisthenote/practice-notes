6ff879119f95f559870454884aa4accbaac5667c
Gabriel <jgj_junior@yahoo.com.br>
2019-07-17 00:29:04 -0300

    submitting a post

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
 import React, { Component } from "react";
+import Posts from "./Posts";
+import PostForm from "./Postform";
 
 export default class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			field: {}
-		};
-	}
-	func = field => {
-		this.setState({
-			field: {
-				...this.state.field,
-				...field
-			}
-		});
-	};
 	render() {
 		return (
 			<div>
-				<Postform toApp={value => this.func(value)} />
-				<p>{JSON.stringify(this.state.field)}</p>
-			</div>
-		);
-	}
-}
-class Postform extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { username: "", password: "" };
-	}
-	change = event => {
-		this.setState({ [event.target.name]: event.target.value });
-		this.props.toApp({ [event.target.name]: event.target.value });
-	};
-	submit = event => {
-		event.preventDefault();
-		console.log(this.state);
-		this.setState({ username: "", password: "" });
-	};
-	render() {
-		return (
-			<div>
-				<form>
-					<input name="username" value={this.state.username} onChange={e => this.change(e)} />
-					<input name="password" value={this.state.password} onChange={e => this.change(e)} />
-					<button onClick={e => this.submit(e)}>Submit</button>
-				</form>
+				<PostForm />
+				<h1>Posts</h1>
+				<Posts />
 			</div>
 		);
 	}
