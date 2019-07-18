16de2cc0464852a669223104e924dee3f3a0b3fe
Gabriel <jgj_junior@yahoo.com.br>
2019-07-18 00:09:10 -0300

    zero

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
-import Posts from "./Posts";
-import PostForm from "./Postform";
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<PostForm />
-				<h1>Posts</h1>
-				<Posts />
+				HELLO
 			</div>
 		);
 	}
