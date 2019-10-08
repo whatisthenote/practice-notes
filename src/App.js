7ffffab2394d6a5067417c617f2baf0f7ec9cf43
Gabriel <jgj_junior@yahoo.com.br>
2019-10-07 22:35:31 -0300

    google auth and storing in firebase example

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
 import React, { Component } from "react";
-import { auth, createUserProfileDocument } from "./Firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUj8zVHoOV69zIki45GCfOXiSHUOO3Nf4",
+  authDomain: "test-d9fad.firebaseapp.com",
+  databaseURL: "https://test-d9fad.firebaseio.com",
+  projectId: "test-d9fad",
+  storageBucket: "test-d9fad.appspot.com",
+  messagingSenderId: "570976355046",
+  appId: "1:570976355046:web:c0d06dcf67e30712b0f127",
+  measurementId: "G-HESQHEYC57"
+};
+firebase.initializeApp(firebaseConfig);
+
+var provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope("profile");
+provider.addScope("email");
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+const signInGoogle = () => firebase.auth().signInWithRedirect(provider);
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: "",
-      displayName: "fdsfdsdfs",
-      email: "fdsf@fadasdaafs.com",
-      password: "122343224"
-    };
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        const userRef = firebase.firestore().doc(`users/${user.uid}`);
+        userRef.get().then(snapshot => {
+          if (!snapshot.exists) {
+            userRef.set({
+              name: user.displayName,
+              email: user.email,
+              createdAt: new Date()
+            });
+          }
+        });
+      }
+    });
   }
-
-  submit = async () => {
-    const { displayName, email, password } = this.state;
-
-    const { user } = await auth.createUserWithEmailAndPassword(email, password);
-    createUserProfileDocument(user, { displayName });
-  };
 
   render() {
     return (
       <div>
-        {console.log(this.state)}
-        <button onClick={this.submit}>submit</button>
-        <button onClick={() => auth.signOut()}>sign out</button>
+        <button onClick={signInGoogle}>google</button>
       </div>
     );
   }
