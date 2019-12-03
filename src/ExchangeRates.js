ae74f2afe25c63364aadd211dbaa4409b1efaa62
Gabriel <jgj_junior@yahoo.com.br>
2019-12-03 00:55:31 -0200

    same exe in props

--- src/ExchangeRates.js
+++ src/ExchangeRates.js
@@ -1,33 +1,33 @@
 import React from "react";
-import { useQuery } from "@apollo/react-hooks";
+import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
 
 const EXCHANGE_RATES = gql`
   {
     rates(currency: "USD") {
+      name
       currency
       rate
-      name
     }
   }
 `;
 
-export default function ExchangeRates() {
-  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+const ExchangeRates = () => (
+  <Query query={EXCHANGE_RATES}>
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
 
-  if (loading) return <p>loading</p>;
-  if (error) return <p>error</p>;
-
-  return (
-    <div>
-      {data.rates.map(({ currency, rate, name }) => (
+      return data.rates.map(({ name, currency, rate }) => (
         <div key={currency}>
           <p>{name}</p>
           <p>{currency}</p>
           <p>{rate}</p>
           <hr />
         </div>
-      ))}
-    </div>
-  );
-}
+      ));
+    }}
+  </Query>
+);
+
+export default ExchangeRates;
